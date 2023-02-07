92d1d7efe083d69fb12f49619c6e0d4177f9c7dd
Brendis21 <bx.millan.e@edu.utc.mx>
2023-02-06 21:48:20 -0600

    Tarea01-InicioSesion

--- Inicio.html/app.js
+++ Inicio.html/app.js
@@ -1,11 +1,11 @@
 function ir(){
 
-    var n=040722;
-    var c="brenda";
-
-    if(document.form.password.value==n && document.form.login.value==c){
-        alert("Usuario y contraseña correctos, bienvenido");}
-        else{
-            alert("Los datos son incorrectos, por favor intente de nuevo");}
-
-        }
+    var c=2805;
+    var u="Brenda";
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("Bienvenido, sus datos son correctos");}
+    else{
+    alert("Los datos ingresados son incorrectos, intenta de nuevo.");
+    }
+    
+    }
