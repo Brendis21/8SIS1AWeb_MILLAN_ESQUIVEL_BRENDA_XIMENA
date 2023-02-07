604fbe91314d88109d30b190f1e25232d3765332
Brendis21 <bx.millan.e@edu.utc.mx>
2023-02-06 21:05:26 -0600

    Tarea01-InicioSesion

---
+++ Inicio.html/app.js
@@ -0,0 +1,11 @@
+function ir(){
+
+    var n=040722;
+    var c="brenda";
+
+    if(document.form.password.value==n && document.form.login.value==c){
+        alert("Usuario y contraseña correctos, bienvenido");}
+        else{
+            alert("Los datos son incorrectos, por favor intente de nuevo");}
+
+        }
